16a394a5ff22869e92d8af65d0dad6612685d246
EC2 Default User <ec2-user@ip-172-31-27-34.eu-west-1.compute.internal>
2019-02-08 09:15:15 +0000

    otputs

--- compute/outputs.tf
+++ compute/outputs.tf
@@ -1,1 +1,8 @@
 #------compute/otputs.tf--------
+output "server_id" {
+	value = "${join(", ", aws_instance.tf_server.*.id)}"
+}
+
+output "server_ip" {
+	value = "${join(", ", aws_instance.tf_server.*.public_ip)}"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,29 @@
 # ----root/outputs.tf------
 
+#storage
 output "Bucket Name" {
     value = "${module.storage.bucketname}"
+}
+
+#networking
+output "Public subnets" {
+	value = "${join(", ", module.networking.public_subnets)}"
+}
+
+output "Subnet IPs" {
+	value = "${join(", ", module.networking.subnet_ips)}"
+}
+
+output "Public security group" {
+	value = "${module.networking.public_sg}"
+}
+
+
+#compute ,join e izpulnen v modula zatva nqma nujda pak
+output "Public Instance IDs" {
+	value = "${module.compute.server_id}"
+}
+
+output "Public Instance IPs" {
+	value = "${module.compute.server_ip}"
 }
